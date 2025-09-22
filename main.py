9642bfa97fcebf0fd17e23e1f7d56bb82f0498cd
𝐀 𝐋 𝐄 𝐗 🐉 <153132171+yassirbili1@users.noreply.github.com>
2025-09-22 23:33:50 +0100

    first commit qe

--- main.py
+++ main.py
@@ -799,36 +799,7 @@ async def dm_member(ctx, member: discord.Member, *, message):
     except Exception as e:
         await ctx.send(f"Failed to DM {member}: {e}")
 
-###########################################
-# make your your voice channel
-###########################################
 
-@bot.command()
-async def play(ctx, url):
-    """Play a song from a URL in the user's voice channel"""
-    if ctx.author.voice is None:
-        await ctx.send("You are not connected to a voice channel.")
-        return
-
-    voice_channel = ctx.author.voice.channel
-
-    if ctx.guild.id not in voice_clients:
-        voice_client = await voice_channel.connect()
-        voice_clients[ctx.guild.id] = voice_client
-        music_queues[ctx.guild.id] = []
-        current_playing[ctx.guild.id] = None
-    else:
-        voice_client = voice_clients[ctx.guild.id]
-        if voice_client.channel != voice_channel:
-            await voice_client.move_to(voice_channel)
-
-    # Add song to queue
-    music_queues[ctx.guild.id].append(url)
-    await ctx.send(f"Added to queue: {url}")
-
-    # If nothing is playing, start playing
-    if current_playing[ctx.guild.id] is None:
-        await play_next(ctx.guild)
 
 
 
