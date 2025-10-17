ced686335b74a2deff3ff27cd056640ce552001c
𝐀 𝐋 𝐄 𝐗 🐉 <153132171+yassirbili1@users.noreply.github.com>
2025-10-17 16:37:03 +0100

    first commit tery

--- main.py
+++ main.py
@@ -1349,8 +1349,8 @@ async def ticket_panel(interaction: discord.Interaction):
         color=discord.Color.blue()
     )
 
-    embed.set_thumbnail(url="https://r2.fivemanage.com/2Fmxtyz3enFCAcfC5wghD/evolutionbanner.png")
-    embed.set_image(url="https://r2.fivemanage.com/2Fmxtyz3enFCAcfC5wghD/evolutionlogotransparantbg.png")
+    embed.set_image(url="https://r2.fivemanage.com/2Fmxtyz3enFCAcfC5wghD/evolutionbanner.png")
+    embed.set_thumbnail(url="https://r2.fivemanage.com/2Fmxtyz3enFCAcfC5wghD/evolutionlogotransparantbg.png")
 
 
     embed.add_field(name="📋 The Button Ticket", value="🔹 Support → If you need general help with the server (rules, features, or how something works).\n\n  💳 Purchase → If you are interested in buying a pack, benefit, or service within the server.\n\n  🐞 Bug → If you found an error, glitch, or bug in the server and want to report it so we can fix it.", inline=False)
